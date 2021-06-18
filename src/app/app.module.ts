08c5ce37dfd073f164610d1c10d0eb14ce51e847
CanRod <candida2295@gmail.com>
2021-06-19 01:21:17 +0530

    added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoFormComponent } from './to-do-form/to-do-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoFormComponent
   ],
   imports: [
     BrowserModule,
